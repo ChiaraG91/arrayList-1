4c2964a72d55b0937d49d588a186afacd0bdfb65
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2023-11-30 22:13:53 +0100

    esercizio caricato

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student studente1 = new Student("Fabio",21);
+        Student studente2 = new Student("Giulia", 20);
+        Student studente3 = new Student("Marco",22);
+
+
+        ArrayList<String> student = new ArrayList<String>();
+        student.add(String.valueOf(studente1));
+        student.add(String.valueOf(studente2));
+        student.add(String.valueOf(studente3));
+        System.out.println(student);
+
+        student.add(String.valueOf(new Student("Fabio",21)));
+        student.add(String.valueOf(new Student("Giulia", 20)));
+        student.add(String.valueOf(new Student("Marco",22)));
+
+        System.out.println(student);
+
+    }
+
+
+
+}
+
---
+++ src/Student.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class Student {
+
+    public String name;
+
+    public Integer age;
+
+    public Student(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void stampaDettagli() {
+        System.out.println("nome:" + name + " " + "età:" + "" + age);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
